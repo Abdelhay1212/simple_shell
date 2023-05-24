2d8a706df5fe05719f8f997f8d47f710c8719d81
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2023-05-24 16:14:10 +0200

    Update _exitShell.c

--- _exitShell.c
+++ _exitShell.c
@@ -2,10 +2,12 @@
 
 /**
  * _exitShell - exit with error number
+ * @command: command to free
  * Return: void
  **/
-void _exitShell(void)
+void _exitShell(char *command)
 {
+	free(command);
 	exit(0);
 }
 
